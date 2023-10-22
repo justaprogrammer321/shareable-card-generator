b996a6f8d5ece561b50f6c09b613ee19dcbdf94a
justaprogrammer321 <architbhrgv@gmail.com>
2023-10-22 16:57:23 +0530

    updated

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const upload = multer({ storage: storage });
 
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'views'));
-app.use( express.static( "public" ) )
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 
